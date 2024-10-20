419e5e548f19c4a2f8b2673e0219a53bb8150be6
Andrey Falkovskiy <avf-91@yandex.ru>
2024-10-20 16:45:38 +0300

    add container with difinitions for instance controller with dependences

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -6,8 +6,9 @@ require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
 use function App\Config\registerRoutes;
+use App\Config\Paths;
 
-$app = new App();
+$app = new App(Paths::SOURCE . "App/container-difinitions.php");
 
 registerRoutes($app);
 
