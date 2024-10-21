90577c9b915715e7a38fa319b543094d5778afae
FalkovskiyAV <FalkovskiyAV@railfgk.ru>
2024-10-21 18:07:31 +0300

    add middleware

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
-use function App\Config\registerRoutes;
+use function App\Config\{registerRoutes, registerMiddleware};
 use App\Config\Paths;
 
 $app = new App(Paths::SOURCE . "App/container-difinitions.php");
 
 registerRoutes($app);
+registerMiddleware($app);
 
 return $app;
--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -7,6 +7,7 @@ namespace Framework;
 class Router
 {
     private array $routes = [];
+    private array $middlewares = [];
 
     public function add(string $method, string $path, array $controller)
     {
@@ -45,5 +46,10 @@ class Router
 
             $controllerInstance->{$function}();
         }
+    }
+
+    public function addMiddleware(string $middleware)
+    {
+        $this->middlewares[] = $middleware;
     }
 }
