40de01f621d6529fe9473c0ef063972449bf9c0d
Andrey Falkovskiy <avf-91@yandex.ru>
2024-10-25 14:46:22 +0300

    add environment-variables

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,8 +5,13 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
-use function App\Config\{registerRoutes, registerMiddleware};
 use App\Config\Paths;
+use Dotenv\Dotenv;
+
+use function App\Config\{registerRoutes, registerMiddleware};
+
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
 
 $app = new App(Paths::SOURCE . "App/container-difinitions.php");
 
