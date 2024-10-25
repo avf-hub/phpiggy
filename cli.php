3cb94c618a09231e4590449be831fdfcfa09833b
Andrey Falkovskiy <avf-91@yandex.ru>
2024-10-25 12:33:32 +0300

    add migration db

--- cli.php
+++ cli.php
@@ -1,14 +1,15 @@
 <?php
 
-$driver = 'mysql';
-$config = http_build_query(data: [
+include __DIR__ . '/src/Framework/Database.php';
+
+use Framework\Database;
+
+$db = new Database('mysql', [
     'host' => 'localhost',
     'port' => 3306,
     'dbname' => 'phpiggy'
-], arg_separator: ';');
+], 'root', '');
 
-$dsn = "{$driver}:{$config}";
-$usernmae = 'root';
-$password = '';
+$sqlFile = file_get_contents("./database.sql");
 
-$db = new PDO($dsn, $username, $password);
+$db->connection->query($sqlFile);
