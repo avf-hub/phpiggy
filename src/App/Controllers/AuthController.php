0f62e2d998a9bdc08c4893dd4df60eab44606584
Andrey Falkovskiy <avf-91@yandex.ru>
2024-10-25 20:25:26 +0300

    add user service for crud operations

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -5,22 +5,39 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use Framework\TemplateEngine;
-use App\Services\ValidatorService;
+use App\Services\{ValidatorService, UserService};
 
 class AuthController
 {
     public function __construct(
         private TemplateEngine $view,
-        private ValidatorService $validatorService
+        private ValidatorService $validatorService,
+        private UserService $userService
     ) {}
 
     public function registerView()
     {
-        echo $this->view->render("/register.php");
+        echo $this->view->render("register.php");
     }
 
     public function register()
     {
         $this->validatorService->validateRegister($_POST);
+
+        $this->userService->isEmailTaken($_POST['email']);
+
+        $this->userService->create($_POST);
+
+        redirectTo('/');
+    }
+
+    public function loginView()
+    {
+        echo $this->view->render("login.php");
+    }
+
+    public function login()
+    {
+        $this->validatorService->validateLogin($_POST);
     }
 }
