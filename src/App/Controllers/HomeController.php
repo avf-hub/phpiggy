004a510f26e5b6a0c3d009f0c935cefcc0b1461b
Andrey Falkovskiy <avf-91@yandex.ru>
2024-10-26 23:41:29 +0300

    add table transactions and output view table dinamicly

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Services\TransactionService;
 use Framework\TemplateEngine;
 
 class HomeController
 {
-    public function __construct(private TemplateEngine $view) {}
+    public function __construct(
+        private TemplateEngine $view,
+        private TransactionService $transactionService
+    ) {}
 
     public function home()
     {
-        echo $this->view->render("/index.php");
+        $transactions = $this->transactionService->getUserTransactions();
+
+        echo $this->view->render("/index.php", ['transactions' => $transactions]);
     }
 }
